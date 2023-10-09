a022b6cb45edd089e12031ab5d1f47c1b37a414c
Walter de Tarso <wtfscampos@fazenda.sp.gov.br>
2023-10-09 14:40:58 -0300

    Folhas de estilo - SCSS

---
+++ modelo_core_mvc/wwwroot/js/progressBar.js
@@ -0,0 +1,13 @@
+function iniciarProgressBar(){
+    let barra = document.getElementById('pb1');
+let carga =0;
+let intBarra=setInterval(()  => {
+    barra.innerHTML = carga +"%";
+barra.style.width =carga + '%';
+carga++;
+if(carga >=100){
+carga =0;
+
+}
+},100);
+}
---
+++ modelo_core_mvc/wwwroot/js/tooltipInit.js
@@ -0,0 +1,4 @@
+function iniciarTooltip() {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+}
