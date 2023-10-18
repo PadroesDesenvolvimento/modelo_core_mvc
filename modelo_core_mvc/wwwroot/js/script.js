9fb3f7f3a4bde60eeb4d3b93ea32cf529a9cd288
Walter de Tarso <wtfscampos@fazenda.sp.gov.br>
2023-10-18 10:22:14 -0300

    SCSS do site

--- modelo_core_mvc/wwwroot/js/script.js
+++ modelo_core_mvc/wwwroot/js/script.js
@@ -1,25 +1,12 @@
-function alternarOculto(elemento) {
-    const menu = document.getElementById(elemento);
-    if (menu.classList.contains('oculto')) {
-        menu.classList.remove('oculto');
-        menu.classList.add('visivel');
-    } else {
-        if (menu.classList.contains('visivel')) {
-            menu.classList.remove('visivel');
+function alternarClasse(elementos, classe) {
+    elementos.forEach(function (elementoId) {
+        const elemento = document.getElementById(elementoId);
+        if (elemento) {
+            elemento.classList.toggle(classe);
+            if (classe === 'recolhido') {
+                elemento.setAttribute('aria-expanded', elemento.classList.contains(classe));
+            }
         }
-        menu.classList.add('oculto');
-    }
+    });
 }
 
-function alternarRecolhido(elemento) {
-    const menu = document.getElementById(elemento);
-    if (menu.classList.contains('recolhido')) {
-        menu.classList.remove('recolhido');
-        menu.classList.add('expandido');
-    } else {
-        if (menu.classList.contains('expandido')) {
-            menu.classList.remove('expandido');
-        }
-        menu.classList.add('recolhido');
-    }
-}
