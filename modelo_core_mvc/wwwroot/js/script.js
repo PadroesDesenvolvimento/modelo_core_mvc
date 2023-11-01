6a5a82c5013c864ccca78654c7516a016c719aba
Walter de Tarso <wtfscampos@fazenda.sp.gov.br>
2023-11-01 16:51:11 -0300

    Botão recolher

--- modelo_core_mvc/wwwroot/js/script.js
+++ modelo_core_mvc/wwwroot/js/script.js
@@ -1,58 +1,38 @@
-function setCookie(cookieName, cookieValue) {
-    document.cookie = cookieName + "=" + cookieValue + "; path=/";
-}
-
-function deleteCookie(cookieName) {
-    document.cookie = cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}
-
-function applyClassesFromCookies() {
-    const classCookies = getAllClassCookies();
-
-    for (const className in classCookies) {
-        const elements = document.getElementsByClassName(className);
-
-        if (elements.length > 0) {
-            elements.forEach(element => {
-                if (classCookies[className] === "true") {
-                    element.classList.add(className);
-                } else {
-                    element.classList.remove(className);
-                }
-            });
-        }
-    }
-}
-
-function getAllClassCookies() {
-    const cookies = document.cookie.split("; ");
-    const classCookies = {};
-
-    cookies.forEach(cookie => {
-        if (cookie.startsWith("class_")) {
-            const [name, value] = cookie.split("=");
-            const className = name.substring(6);
-            classCookies[className] = value;
-        }
-    });
-
-    return classCookies;
-}
-
-function alternarClasse(elementos, classe) {
+function alternarClasse(elementos, classe, gravarCookie=true) {
     elementos.forEach(function (elementoId) {
         const elemento = document.getElementById(elementoId);
         if (elemento) {
             elemento.classList.toggle(classe);
-
-            if (elemento.classList.contains(classe)) {
-                setCookie("class_" + elementoId, "true");
-            } else {
-                deleteCookie("class_" + elementoId);
+            if (gravarCookie) {
+                document.cookie = "classe|" + elementoId + "|" + classe + "=" + elemento.classList.contains(classe) + "; path=/";
             }
         }
     });
 }
 
-// Chame esta função ao carregar a página
-applyClassesFromCookies();
+function aplicarClassesDeCookies() {
+    var cookies = document.cookie.split("; ");
+    cookies.forEach(cookie => {
+        if (cookie.startsWith("classe|")) {
+            var nome = cookie.split("=")[0];
+            var elementoId = nome.split("|")[1];
+            var classe = nome.split("|")[2];
+            var elemento = document.getElementById(elementoId);
+            if (elemento) {
+                var ativar = cookie.split("=")[1].split(";")[0];
+                if (elemento.classList.contains(classe)) {
+                    if (ativar === "false") {
+                        elemento.classList.remove(classe);
+                    };
+                }
+                else {
+                    if (ativar === "true") {
+                        elemento.classList.add(classe);
+                    }
+                }
+            }
+        }
+    });
+}
+
+aplicarClassesDeCookies();
