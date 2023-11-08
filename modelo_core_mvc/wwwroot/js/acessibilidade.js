9ee83a59aa1c29561aba86779eb8c1dcbbe422a5
Walter de Tarso <wtfscampos@fazenda.sp.gov.br>
2023-11-08 17:36:07 -0300

    Ajuste do Zoom

--- modelo_core_mvc/wwwroot/js/acessibilidade.js
+++ modelo_core_mvc/wwwroot/js/acessibilidade.js
@@ -1,79 +1,88 @@
 let valorMudanca = 0;
+let valores = [1, 1.1, 1.3, 1.5, 1.75, 1.9]
+function atualizarBotoesZoom() {
+    const buttons = document.querySelectorAll('.botaoZoom');
+    buttons.forEach(function (button) {
+        const ariaLabel = button.getAttribute('aria-label');
+        const zoomValue = Math.round(valores[valorMudanca] * 100);
+        button.title = `${ariaLabel} - ${zoomValue}%`;
+    });
+};
 
-// ***************************************************************************************************************************************//
-//                                                      Aumentar caractere                                                                //
-// ***************************************************************************************************************************************//
-
-function aumentarCaracter(){
-  let valores = [1,1.1,1.3,1.5,1.75,1.9]
-  let alturaMenuLateral = ['0','0','1px','-125px','-241px']
-  valorMudanca = valorMudanca + 1;
-  if(valorMudanca < 0) {valorMudanca = 0}
-  else if(valorMudanca > 5) {valorMudanca = 5}
-  document.getElementsByTagName("body").item(0).style.setProperty('zoom', valores[valorMudanca])
+function aumentarCaracter() {
+    valorMudanca = valorMudanca + 1;
+    if (valorMudanca < 0) { valorMudanca = 0 }
+    else if (valorMudanca > 5) { valorMudanca = 5 }
+    document.cookie = "valorMudanca=" + valorMudanca + "; path=/";
+    document.getElementsByTagName("body").item(0).style.setProperty('zoom', valores[valorMudanca]);
+    atualizarBotoesZoom()
 }
 
-// ***************************************************************************************************************************************//
-//                                                      Dimiuir caractere                                                                 //
-// ***************************************************************************************************************************************//
-
-function diminuirCaracter(){
-  let valores = [1,1.1,1.3,1.5,1.75,1.9]
-  let alturaMenuLateral = ['0','0','1px','-125px','-241px']
-  valorMudanca = valorMudanca - 1;
-  if(valorMudanca < 0) {valorMudanca = 0}
-  else if(valorMudanca > 5) {valorMudanca = 5}
-  document.getElementsByTagName("body").item(0).style.setProperty('zoom', valores[valorMudanca])
+function diminuirCaracter() {
+    valorMudanca = valorMudanca - 1;
+    if (valorMudanca < 0) { valorMudanca = 0 }
+    else if (valorMudanca > 5) { valorMudanca = 5 }
+    document.cookie = "valorMudanca=" + valorMudanca + "; path=/";
+    document.getElementsByTagName("body").item(0).style.setProperty('zoom', valores[valorMudanca]);
+    atualizarBotoesZoom()
 }
+
 
 // ***************************************************************************************************************************************//
 //                                                        Alto contrste                                                                   //
 // ***************************************************************************************************************************************//
 (function () {
-  var Contrast = {
-      storage: 'contrastState',
-      cssClass: 'color-blind',
-      currentState: null,
-      check: checkContrast,
-      getState: getContrastState,
-      setState: setContrastState,
-      toogle: toogleContrast,
-      updateView: updateViewContrast
-  };
+    var Contrast = {
+        storage: 'contrastState',
+        cssClass: 'color-blind',
+        currentState: null,
+        check: checkContrast,
+        getState: getContrastState,
+        setState: setContrastState,
+        toogle: toogleContrast,
+        updateView: updateViewContrast
+    };
 
-  window.toggleContrast = function () { Contrast.toogle(); };
+    window.toggleContrast = function () { Contrast.toogle(); };
 
-  Contrast.check();
+    Contrast.check();
 
-  function checkContrast() {
-      this.updateView();
-  }
+    function checkContrast() {
+        this.updateView();
+    }
 
-  function getContrastState() {
-      return localStorage.getItem(this.storage) === 'true';
-  }
+    function getContrastState() {
+        return localStorage.getItem(this.storage) === 'true';
+    }
 
-  function setContrastState(state) {
-      localStorage.setItem(this.storage, '' + state);
-      this.currentState = state;
-      this.updateView();
-  }
+    function setContrastState(state) {
+        localStorage.setItem(this.storage, '' + state);
+        this.currentState = state;
+        this.updateView();
+    }
 
-  function updateViewContrast() {
-      var body = document.body;
-      
-      if (!body) return;
+    function updateViewContrast() {
+        var body = document.body;
 
-      if (this.currentState === null)
-          this.currentState = this.getState();
+        if (!body) return;
 
-      if (this.currentState)
-          body.classList.add(this.cssClass);
-      else
-          body.classList.remove(this.cssClass);
-  }
+        if (this.currentState === null)
+            this.currentState = this.getState();
 
-  function toogleContrast() {
-      this.setState(!this.currentState);
-  }
+        if (this.currentState)
+            body.classList.add(this.cssClass);
+        else
+            body.classList.remove(this.cssClass);
+    }
+
+    function toogleContrast() {
+        this.setState(!this.currentState);
+    }
 })();
+
+window.onload = function () {
+    var body = document.getElementsByTagName("body")[0];
+    var valorZoom = window.getComputedStyle(body).getPropertyValue('zoom');
+    valorMudanca = valores.indexOf(parseFloat(valorZoom));
+    atualizarBotoesZoom()
+}
