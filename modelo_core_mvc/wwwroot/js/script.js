c97eef7108a44636bf0993f353e23a7a3d5fa280
Walter de Tarso <wtfscampos@fazenda.sp.gov.br>
2023-11-08 10:35:55 -0300

    Tabelas

--- modelo_core_mvc/wwwroot/js/script.js
+++ modelo_core_mvc/wwwroot/js/script.js
@@ -10,33 +10,6 @@ function alternarClasse(elementos, classe, gravarCookie=true) {
     });
 }
 
-function aplicarClassesDeCookies() {
-    var cookies = document.cookie.split("; ");
-    cookies.forEach(cookie => {
-        if (cookie.startsWith("classe|")) {
-            var nome = cookie.split("=")[0];
-            var elementoId = nome.split("|")[1];
-            var classe = nome.split("|")[2];
-            var elemento = document.getElementById(elementoId);
-            if (elemento) {
-                var ativar = cookie.split("=")[1].split(";")[0];
-                if (elemento.classList.contains(classe)) {
-                    if (ativar === "false") {
-                        elemento.classList.remove(classe);
-                    };
-                }
-                else {
-                    if (ativar === "true") {
-                        elemento.classList.add(classe);
-                    }
-                }
-            }
-        }
-    });
-}
-
-//window.addEventListener('DOMContentLoaded', aplicarClassesDeCookies());
-
 function _aguarde(mostrar=true) {
     var elemento = document.getElementById('_aguarde');
     if (elemento) {
