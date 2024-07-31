215733651260eecee48bb25b27bb58f15f47802b
Walter de Tarso <wtfscampos@fazenda.sp.gov.br>
2024-07-31 10:48:31 -0300

    CDN #218208

--- modelo_core_mvc/Controllers/AutenticacaoAPIController.cs
+++ modelo_core_mvc/Controllers/AutenticacaoAPIController.cs
@@ -1,176 +1,179 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using SefazLib.IdentityCfg;
-using System.Linq;
-using System.Xml;
 using SefazLib.SamlToJwt;
 using System;
-using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
-using System.Text;
-using Microsoft.AspNetCore.Authentication;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 using System.Web;
+using System.Xml;
 
-namespace exemplo_autenticador_bff.Controllers;
-
-[Route("[controller]")]
-[ApiController]
-public class AutenticacaoAPIController : ControllerBase
+namespace exemplo_autenticador_bff.Controllers
 {
-    private readonly IConfiguration configuration;
-    private readonly ILogger<AutenticacaoAPIController> logger;
-    private readonly IdentityConfig identityConfig;
-
-    public AutenticacaoAPIController(IConfiguration Configuration,
-                                  ILogger<AutenticacaoAPIController> Logger,
-                                  IdentityConfig IdentityConfig)
+    [Route("[controller]")]
+    [ApiController]
+    public class AutenticacaoAPIController : ControllerBase
     {
-        configuration = Configuration;
-        logger = Logger;
-        identityConfig = IdentityConfig;
-    }
+        private readonly IConfiguration configuration;
+        private readonly ILogger<AutenticacaoAPIController> logger;
+        private readonly IdentityConfig identityConfig;
 
-    [Authorize]
-    [HttpGet("token")]
-    public async Task<IActionResult> Obter_Token(string returnUrl)
-    {
-        var token = "";
-
-        if (User.Identity.IsAuthenticated)
+        public AutenticacaoAPIController(IConfiguration Configuration, ILogger<AutenticacaoAPIController> Logger, IdentityConfig IdentityConfig)
         {
-            if (configuration["identity:type"] == "azuread")
-            {
-                try
-                {
-                    token = await identityConfig.obterAccessToken(null);
-                }
-                catch { }
-            }
-            else
-            {
-                var claims = User.Claims;
-                foreach (var claim in User.Claims)
-                {
-                    if (claim.Type.Contains("id_token")) { token = claim.Value; }
-                }
+            configuration = Configuration;
+            logger = Logger;
+            identityConfig = IdentityConfig;
+        }
 
-                if (token == "")
-                {
-                    var xml = new XmlDocument();
-                    var bootstapContext = (string)User.Identities.First().BootstrapContext;
-                    xml.LoadXml(bootstapContext);
-                    var innerXml = xml.InnerXml;
-                    var assertionNode = xml.SelectSingleNode("//*[local-name()='Assertion']");
+        [Authorize]
+        [HttpGet("token")]
+        public async Task<IActionResult> Obter_Token(string returnUrl)
+        {
+            var token = "";
 
-                    if (assertionNode != null)
+            if (User.Identity.IsAuthenticated)
+            {
+                if (configuration["identity:type"] == "azuread")
+                {
+                    try
                     {
-                        string samlToken = assertionNode.OuterXml;
-                        var jwtToken = SamlToJwt.ConvertSamlToJwt(samlToken, configuration);
-                        token = jwtToken;
+                        token = await identityConfig.obterAccessToken(null);
                     }
-                }   
-            }
-        }
-        else
-        {
-            return Unauthorized();
-        }
+                    catch { }
+                }
+                else
+                {
+                    var claims = User.Claims;
+                    foreach (var claim in User.Claims)
+                    {
+                        if (claim.Type.Contains("id_token")) { token = claim.Value; }
+                    }
 
-        if (!string.IsNullOrEmpty(returnUrl))
-        {
-            var uriBuilder = new UriBuilder(returnUrl);
-            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
-            query["token"] = token;
-            uriBuilder.Query = query.ToString();
-            return Redirect(uriBuilder.ToString());
-        }
+                    if (token == "")
+                    {
+                        var xml = new XmlDocument();
+                        var bootstapContext = (string)User.Identities.First().BootstrapContext;
+                        xml.LoadXml(bootstapContext);
+                        var assertionNode = xml.SelectSingleNode("//*[local-name()='Assertion']");
 
-        return Ok(new { token });
-    }
-
-    [HttpGet("teste")]
-    public async Task<IActionResult> Teste_chamada()
-    {
-        var token = "Ok";
-        return Ok(new { token });
-    }
-    
-    [HttpGet("validar-token")]
-    [Authorize]
-    public IActionResult ValidarToken()
-    {
-        // Obter o token do cabeçalho Authorization
-        string authorizationHeader = HttpContext.Request.Headers["Authorization"];
-
-        if (string.IsNullOrEmpty(authorizationHeader))
-        {
-            return BadRequest("Token JWT não encontrado no cabeçalho Authorization.");
-        }
-
-        // O cabeçalho Authorization tem o formato "Bearer {token}", onde {token} é o token JWT
-        string[] tokenParts = authorizationHeader.Split(' ');
-
-        if (tokenParts.Length != 2 || !tokenParts[0].Equals("Bearer", StringComparison.OrdinalIgnoreCase))
-        {
-            return BadRequest("Formato inválido do cabeçalho Authorization. Use 'Bearer {token}'.");
-        }
-
-        string jwtToken = tokenParts[1];
-
-        try
-        {
-            // Aqui você pode adicionar a lógica para validar o token JWT
-            // Pode ser uma verificação de assinatura, expiração, reivindicações, etc.
-            // Se o token for válido, retorne OK, caso contrário, retorne Unauthorized
-
-            // Exemplo de validação simples
-            if (IsValidToken(jwtToken, ""))
-            {
-                return Ok("Token JWT válido.");
+                        if (assertionNode != null)
+                        {
+                            string samlToken = assertionNode.OuterXml;
+                            var jwtToken = SamlToJwt.ConvertSamlToJwt(samlToken, configuration);
+                            token = jwtToken;
+                        }
+                    }
+                }
             }
             else
             {
-                return Unauthorized("Token JWT inválido.");
+                return Unauthorized();
+            }
+
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                var uriBuilder = new UriBuilder(returnUrl);
+                var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+                query["token"] = token;
+                uriBuilder.Query = query.ToString();
+                return Redirect(uriBuilder.ToString());
+            }
+
+            return Ok(new { token });
+        }
+
+        [HttpGet("validar-token")]
+        public IActionResult ValidarToken()
+        {
+            // Obter o token do cabeçalho Authorization
+            string authorizationHeader = HttpContext.Request.Headers["Authorization"];
+
+            if (string.IsNullOrEmpty(authorizationHeader))
+            {
+                return BadRequest("Token JWT não encontrado no cabeçalho Authorization.");
+            }
+
+            // O cabeçalho Authorization tem o formato "Bearer {token}", onde {token}" é o token JWT
+            string[] tokenParts = authorizationHeader.Split(' ');
+
+            if (tokenParts.Length != 2 || !tokenParts[0].Equals("Bearer", StringComparison.OrdinalIgnoreCase))
+            {
+                return BadRequest("Formato inválido do cabeçalho Authorization. Use 'Bearer {token}'.");
+            }
+
+            string jwtToken = tokenParts[1];
+
+            try
+            {
+                if (IsValidToken(jwtToken))
+                {
+                    return Ok("Token JWT válido.");
+                }
+                else
+                {
+                    return Unauthorized("Token JWT inválido.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro ao validar o token JWT: {ex.Message}");
             }
         }
-        catch (Exception ex)
-        {
-            return StatusCode(500, $"Erro ao validar o token JWT: {ex.Message}");
-        }
-    }
 
-    private bool IsValidToken(string jwtToken, string publicKey)
-    {
-        try
+        private string SignToken(string token)
         {
-            // Configurar o validador de tokens JWT
+            var privateKey = configuration["identity:PrivateKey"];
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(privateKey);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = new RsaSecurityKey(rsa);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("jwt", token) }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256)
+            };
+
+            var signedToken = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(signedToken);
+        }
+
+        private bool IsValidToken(string jwtToken)
+        {
+            var publicKey = configuration["identity:PublicKey"];
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(publicKey);
+
             var tokenValidationParameters = new TokenValidationParameters
             {
-                ValidateIssuer = false, // Configure de acordo com suas necessidades
-                ValidateAudience = false, // Configure de acordo com suas necessidades
+                ValidateIssuer = false,
+                ValidateAudience = false,
                 ValidateLifetime = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(publicKey))
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new RsaSecurityKey(rsa)
             };
 
             var tokenHandler = new JwtSecurityTokenHandler();
             SecurityToken securityToken;
 
-            // Tente validar o token JWT
-            var claimsPrincipal = tokenHandler.ValidateToken(jwtToken, tokenValidationParameters, out securityToken);
-
-            // Se a validação não lançou exceções, o token é válido
-            return true;
-        }
-        catch (Exception ex)
-        {
-            // Trate exceções de validação aqui, se necessário
-            // Por exemplo, TokenExpiredException, SecurityTokenValidationException, etc.
-            Console.WriteLine(ex.Message);
-            return false;
+            try
+            {
+                tokenHandler.ValidateToken(jwtToken, tokenValidationParameters, out securityToken);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Erro ao validar o token: {Message}", ex.Message);
+                return false;
+            }
         }
     }
-
 }
