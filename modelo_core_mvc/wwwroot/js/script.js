ebb2342617063b9c95773a1880ac32e962d114d7
Walter de Tarso <wtfscampos@fazenda.sp.gov.br>
2024-11-01 11:34:14 -0300

    Menu lateral

--- modelo_core_mvc/wwwroot/js/script.js
+++ modelo_core_mvc/wwwroot/js/script.js
@@ -1,16 +1,27 @@
-function alternarClasse(elementos, classe, gravarCookie=true) {
+function setCookie(name, value, days=7) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function alternarClasse(elementos, classe, gravarCookie = true, days = 7) {
     elementos.forEach(function (elementoId) {
         const elemento = document.getElementById(elementoId);
         if (elemento) {
             elemento.classList.toggle(classe);
             if (gravarCookie) {
-                document.cookie = "classe|" + elementoId + "|" + classe + "=" + elemento.classList.contains(classe) + "; path=/";
+                const cookieName = "classe|" + elementoId + "|" + classe;
+                const cookieValue = elemento.classList.contains(classe);
+                setCookie(cookieName, cookieValue, days);
             }
         }
     });
 }
 
-function _aguarde(mostrar=true) {
+function _aguarde(mostrar = true) {
     var elemento = document.getElementById('_aguarde');
     if (elemento) {
         if (elemento.classList.contains('oculto')) {
