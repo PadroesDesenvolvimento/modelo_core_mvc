bc8f2bfaafadc257b4070cbb64a9ed0f6e156940
Walter de Tarso <wtfscampos@fazenda.sp.gov.br>
2024-11-01 17:20:32 -0300

    Menu lateral

--- modelo_core_mvc/wwwroot/js/script.js
+++ modelo_core_mvc/wwwroot/js/script.js
@@ -7,6 +7,18 @@ function setCookie(name, value, days=7) {
     }
     document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
 function alternarClasse(elementos, classe, gravarCookie = true, days = 7) {
     elementos.forEach(function (elementoId) {
         const elemento = document.getElementById(elementoId);
@@ -38,6 +50,33 @@ function _aguarde(mostrar = true) {
 }
 
 window.addEventListener('load', _aguarde(false));
+function _selecionar() {
+    const menuItems = document.querySelectorAll('.botaoMenu');
 
+    // Restaurar o item selecionado do cookie
+    const itemSelecionado = getCookie('itemSelecionado');
+    if (itemSelecionado) {
+        const itemMenu = Array.from(menuItems).find(item => item.outerText === itemSelecionado);
+        if (itemMenu) {
+            itemMenu.classList.add('selecionado');
+        }
+    }
 
+    menuItems.forEach(item => {
+        item.addEventListener('click', function () {
+            // Remover a classe "selecionado" de todos os itens
+            menuItems.forEach(i => i.classList.remove('selecionado'));
 
+            // Adicionar a classe "selecionado" ao item clicado
+            this.classList.add('selecionado');
+
+            // Salvar o estado do item selecionado em um cookie
+            const action = this.outerText;
+            if (action) {
+                setCookie('itemSelecionado', action, 7);
+            }
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', _selecionar);
